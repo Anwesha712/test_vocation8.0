dde2a7278f49b93ca7f99628149ad29253d1cef0
Riyanshu Baskey <115870286+iam-rbaskey@users.noreply.github.com>
2024-03-18 02:07:52 +0530

    I am riyanshu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Hero2 from "./Hero2";
+import Home from "./Home";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import Registration1 from "./Registration1";
+import Contact from "./Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      
+      <Routes><Route path="/" Component={Home} /></Routes>
+      <Routes><Route path="/Registration" Component={Registration1} /></Routes>
+      <Routes><Route path="/Connect" Component={Contact} /></Routes>
     </div>
   );
 }
