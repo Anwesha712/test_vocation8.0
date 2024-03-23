f0fa4398cd603f1ef90a8e4ade15d8ed31eba681
Anwesha Sen <senanwesha690@gmail.com>
2024-03-23 16:47:04 +0530

    Show more added

--- src/Hero2.js
+++ src/Hero2.js
@@ -5,11 +5,9 @@ import Gallery from './gallery'
 
 const Hero2 = () => {
   return (
-    <div className=' bg-bg1 h-screen w-full items-center justify-center text-white flex'>
-     <div className='p-4 bg-black h-5/5 w-4/4 shadow-2xl items-center justify-center '>
+    <div className='p-4 bg-black h-5/5 w-4/4 shadow-2xl items-center justify-center '>
       <Gallery/>
     </div> 
-    </div>
   )
 }
 
--- src/gallery.js
+++ src/gallery.js
@@ -11,6 +11,10 @@ import Img7 from './assets/Img7.jpeg';
 import Img8 from './assets/Img8.jpeg';
 import Img9 from './assets/Img9.jpeg';
 import Img10 from './assets/Img10.jpeg';
+import Img11 from './assets/Img11.jpeg';
+import Img12 from './assets/Img12.jpeg';
+import Img13 from './assets/Img13.jpeg';
+import Img14 from './assets/Img14.jpeg';
 
 const Gallery = () => {
 
@@ -54,6 +58,22 @@ const Gallery = () => {
     {
         id: 10,
         imgSrc: Img10,
+    },
+    {
+        id: 11,
+        imgSrc: Img11,
+    },
+    {
+        id: 12,
+        imgSrc: Img12,
+    },
+    {
+        id: 13,
+        imgSrc: Img13,
+    },
+    {
+        id: 14,
+        imgSrc: Img14,
     }
     ]
 
@@ -63,6 +83,10 @@ const Gallery = () => {
         setTemp(imgSrc);
         setModel(true);
     }
+    const[visible,setVisible] = useState(10);
+    const showMore = ()=>{
+        setVisible((prevValue)=> prevValue + 4);
+    };
 
     return(
         <>
@@ -71,7 +95,7 @@ const Gallery = () => {
             <img src={Close} onClick={()=> setModel(false)} className='close' style={{width:'30px'}} />
         </div>
          <div className="gallery">
-         {data.map((item)=>{
+         {data.slice(0,visible).map((item)=>{
                 return(
                     <div className="pics" key={item.id} onClick={()=> getImg(item.imgSrc)}>
                         <img src={item.imgSrc} style={{width:'100%' }}/>
@@ -82,6 +106,10 @@ const Gallery = () => {
          }
 
         </div>
+        <div className="btn_show">
+            <button onClick={showMore} className="bg-gradient-to-r from-[#3e3b92] to-[#f44369]">Show More</button>
+         </div>
+        
         </>
     )
 }
