03c4907085acd6f1d365d6b45806a0d2f78d7460
Riyanshu Baskey <115870286+iam-rbaskey@users.noreply.github.com>
2024-03-27 00:39:12 +0530

    I am rbaskey

--- src/Footer.js
+++ src/Footer.js
@@ -7,10 +7,9 @@ const Footer = () => {
     <div className="w-full bg-bg2 px-8 md:px-12 lg:px-36 box-border ">
       <div className="inline-flex flex-col md:flex-row w-full h-full align-middle justify-center py-10 gap-4">
         <div className="md:w-1/2 lg:1/3 text-wrap text-white font-font3 my-auto">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis
-          adipisci dolorem corporis repudiandae rem, cupiditate, quas vitae
-          debitis, magnam molestiae iure sequi. Molestias, laborum? Adipisci
-          animi magnam consequuntur sint vero.
+        SARGAM
+The pulsating heart and the official Music Club of MSIT 
+Here harmonizing talents , fostering creativity and elevating campus melodies. From vibrant jam sessions to soul stirring performances, where each chord strummed and lyric sung reflects the vibrant spirit of our college's musical culture.
         </div>
         <hr className="w-full h-[2px] md:h-32 my-auto md:w-[2px] bg-white text-white " />
         <div className="text-white text-xl font-semibold font-font1 inline-flex flex-col gap-2 justify-center text-center">
