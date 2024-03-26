2cf47fc0278bf604ff90f7b75cfe3c2799d054e5
Sammonnay-Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2024-03-27 00:45:59 +0530

    idk3

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
-import Hero2 from "./Hero2";
 import GalleryTW from "./GalleryTW";
 import Member from "./Member";
 import Footer from "./Footer";
 import Event from "./Event";
-import Up_event from "./Up_event";
+import UpEvent from "./Up_event";
 
 const Home = () => {
   return (
@@ -18,11 +17,12 @@ const Home = () => {
           <Event />
         </div>
         <GalleryTW />
+
+        <div className="p-4 bg-black h-5/5 w-4/4 shadow-2xl items-center justify-center ">
+          <UpEvent />
+        </div>
         <div className="p-4 bg-black h-fit w-full shadow-2xl items-center justify-center ">
           <Member />
-        </div>
-        <div className="p-4 bg-black h-5/5 w-4/4 shadow-2xl items-center justify-center ">
-          <Up_event />
         </div>
         <Footer />
       </div>
